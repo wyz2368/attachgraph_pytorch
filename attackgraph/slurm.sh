c6f83971eb7c4d47c91281a1895ee3a9ee89f1e2
wangyzh <wangyzh@umich.edu>
2020-05-29 00:05:48 -0400

    "remove loading strategies."

--- attackgraph/slurm.sh
+++ attackgraph/slurm.sh
@@ -5,10 +5,10 @@
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --mem-per-cpu=14g
-#SBATCH --time=03-00:00:00
+#SBATCH --mem-per-cpu=40g
+#SBATCH --time=10-00:00:00
 #SBATCH --account=wellman1
-#SBATCH --partition=standard
+#SBATCH --partition=largemem
 
 module load python3.6-anaconda/5.2.0
 cd ${SLURM_SUBMIT_DIR}
